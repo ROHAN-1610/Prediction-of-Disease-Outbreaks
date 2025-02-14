c72a0011ce86047cb884962994887ae95c147bef
ROHAN VERMA <vrohan1610@gmail.com>
2025-02-14 15:39:27 +0530

    Updated

--- .history/app_20250213022129.py
+++ .history/app_20250213022129.py
@@ -74,9 +74,9 @@ st.markdown("""
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Loading the saved models
-diabetes_model = pickle.load(open(f'{working_dir}/training_models/diabetes_model.sav', 'rb'))
-heart_disease_model = pickle.load(open(f'{working_dir}/training_models/heart_disease_model.sav', 'rb'))
-parkinsons_model = pickle.load(open(f'{working_dir}/training_models/parkinsons_model.sav', 'rb'))
+diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
+parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
 
 # Sidebar for navigation
 with st.sidebar:
